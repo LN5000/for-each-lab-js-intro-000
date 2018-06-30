09088aab18f80333143b1a65c7dff4f8b642c686
Lee <lee.o.noble@gmail.com>
2018-06-30 08:54:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-function iterativeLog(array) {
-  array.forEach(function(element) {
-      console.log(`${array.indexOf(element)}: ${element}`);
-  });
-}
+
 
 iterate(callback) {
   var internal = [1,2,3,4,5,6,7];
